b2ebf84fca85ca80ce154534467907b893c4a63f
umatbro <umatbroo@gmail.com>
2018-02-22 20:39:28 +0100

    zadDnia1

--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,18 @@
-//Twój kod
+let fs = require('fs');
+let crypto = require('crypto');
+
+let filePath = process.argv[2];
+
+if (filePath === undefined) {
+    console.log(`Please provide path to a file`);
+    process.exit(1);
+}
+
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) console.log(err);
+    else {
+        // console.log(data);
+        console.log(crypto.createHmac('sha256', data).digest('hex'));
+    }
+});
